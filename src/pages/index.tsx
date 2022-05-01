5de8f6a7046e6fc3d32878a3324fd82145df38ab
Raoul Bivolaru <raoul.bivolaru@gmail.com>
2022-05-01 22:31:30 +0300

    add helmet

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,10 +3,19 @@ import Hero from 'sections/Hero';
 import Layout from 'components/Layout';
 import React from 'react';
 import Contact from 'sections/Contact';
+import { Helmet } from 'react-helmet';
 
 export default function () {
   return (
     <Layout>
+      <Helmet>
+        <title>raoul.tech</title>
+        <meta name="author" content="Raoul Bivolaru" />
+        <meta
+          name="description"
+          content="Fullstack digital freelancer. Experienced developer with over 10 years of experience in the domain."
+        />
+      </Helmet>
       <Hero />
       <About />
       <Contact />
