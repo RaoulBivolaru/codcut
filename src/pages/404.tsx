95bff58837d56cc58a20d7c772301752939ed32e
Raoul Bivolaru <raoul.bivolaru@gmail.com>
2022-05-01 16:48:19 +0300

    style 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,6 +1,19 @@
+import Button from 'components/Button';
 import Layout from 'components/Layout';
+import { Link } from 'gatsby';
 import React from 'react';
 
 export default function () {
-  return <Layout>NOT FOUND</Layout>;
+  return (
+    <Layout>
+      <section className="h-screen bg-brand flex items-center justify-center">
+        <div className="items-center flex flex-col">
+          <h1 className="text-accent text-2xl font-bold mb-2">NOT FOUND</h1>
+          <Link to="/" replace>
+            <Button label="Go home" onClick={() => {}} />
+          </Link>
+        </div>
+      </section>
+    </Layout>
+  );
 }
