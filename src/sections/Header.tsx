1dcfa5be8d32767958ff9b1e0a12c506ee5d13f1
Raoul Bivolaru <contact@raoul.tech>
2022-10-02 23:43:25 +0300

    Fix logo

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -8,7 +8,7 @@ import Image from 'components/Image';
 export default function () {
   return (
     <header className="p-10 flex justify-between items-center absolute right-0 left-0 top-0 z-10">
-      <Image src="logo.svg" alt="logo" w={120} />
+      <Image src="/logo.svg" alt="logo" w={120} />
       <HeaderNavigation />
       <Button
         label="Let's Talk"
