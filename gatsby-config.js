bda34ca20db24e0d8bad6c53044987e1939f41aa
Raoul Bivolaru <contact@raoul.tech>
2022-10-07 18:27:33 +0300

    Update seo

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,9 +5,11 @@ const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE;
 module.exports = {
   siteMetadata: {
     title: `raoul.tech`,
-    headline: `Fullstack digital freelancer`,
+    headline: `IT services and consulting provider`,
     url: `https://raoul.tech`,
-    description: `Fullstack digital freelancer. Experienced developer with over 10 years of experience in the field`,
+    description: `IT studio with over 10 years of experience in the field. Carry out
+    dozen of projects like UI/UX integration, SSO implementation,
+    payment integration, API creation.`,
     language: `en`,
     image: `https://raoul.tech/logo.png`,
     author: `Raoul Bivolaru`,
