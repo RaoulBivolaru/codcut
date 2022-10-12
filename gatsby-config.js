45a6c598e794fa075154dad37d5b3cdcd62b2dc8
Raoul Bivolaru <contact@raoul.tech>
2022-10-12 17:52:15 +0300

    Add manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,6 +29,34 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'raoul.tech | IT Studio',
+        short_name: 'raoul.tech',
+        start_url: '/',
+        lang: 'en',
+        icon: '/logo.png',
+        description: `IT studio with over 10 years of experience in the field. Carry out
+        dozen of projects like UI/UX integration, SSO implementation,
+        payment integration, API creation.`,
+        icons: [
+          {
+            src: '/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+        theme_color: '#f9004d',
+        background_color: '#101010',
+        display: 'standalone',
+      },
+    },
+    {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
         fonts: [
@@ -36,6 +64,12 @@ module.exports = {
           `Montserrat\:100,300,400,700,900`,
         ],
         display: 'swap',
+      },
+    },
+    {
+      resolve: `gatsby-plugin-offline`,
+      options: {
+        precachePages: [`/`, '/privacy'],
       },
     },
     shouldAnalyseBundle && {
