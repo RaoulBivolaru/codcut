7be47fe62163f993ec2b348099b4cb06cad21717
Raoul Bivolaru <contact@raoul.tech>
2022-10-16 18:44:16 +0300

    Disable zoom

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -2,19 +2,20 @@ import { graphql, useStaticQuery } from 'gatsby';
 import React from 'react';
 import { Helmet } from 'react-helmet';
 
-const query = graphql`query SEO {
+const query = graphql`
+  query SEO {
     site {
-        siteMetadata {
-            title
-            description
-            url
-            image
-            author
-            headline
-        }
+      siteMetadata {
+        title
+        description
+        url
+        image
+        author
+        headline
+      }
     }
-}`;
-
+  }
+`;
 
 const SEO = () => {
   const {
@@ -27,12 +28,13 @@ const SEO = () => {
         lang: 'en',
       }}>
       <title>{meta.title}</title>
-      <meta name='author' content={meta.author} />
-      <meta name='description' content={meta.description} />
-      <meta property='og:title' content={meta.headline} />
-      <meta property='og:image' content={meta.image} />
-      <meta property='og:url' content={meta.url} />
-      <meta property='og:site_name' content={meta.title} />
+      <meta name="author" content={meta.author} />
+      <meta name="description" content={meta.description} />
+      <meta property="og:title" content={meta.headline} />
+      <meta property="og:image" content={meta.image} />
+      <meta property="og:url" content={meta.url} />
+      <meta property="og:site_name" content={meta.title} />
+      <meta name="viewport" content="width=device-width, user-scalable=no" />
     </Helmet>
   );
 };
