986a8e99cdce0440fbea60afcb36d18df0d5b5c8
Raoul Bivolaru <contact@raoul.tech>
2023-01-24 13:26:58 +0200

    Add new projects

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -9,34 +9,26 @@ interface ProjectProps {
   url: string;
 }
 
-const Project: FC<ProjectProps> = (props) => {
+const Project: FC<ProjectProps> = props => {
   const { image, title, meta, url } = props;
   const { width } = useWindowSize();
 
-  const imagePath =
-    width <= BREAKPOINT.SMALLER_MOBILE
-      ? image.replace('.png', '-mobile.png')
-      : image;
+  const imagePath = width <= BREAKPOINT.SMALLER_MOBILE ? image.replace('.png', '-mobile.png') : image;
 
   return (
-    <a
-      href={url}
-      rel='nofollow'
-      target='_blank'
-      className='w-full rounded-xl hover:scale-105 transition-all group'>
-      <article className='w-full h-96 relative flex justify-center items-end rounded-xl overflow-hidden'>
+    <a href={url} rel="nofollow" target="_blank" className="w-full rounded-xl hover:scale-105 transition-all group">
+      <article className="w-full h-96 relative flex justify-center items-end rounded-xl overflow-hidden">
         <img
           src={imagePath}
           alt={title}
-          className='object-cover absolute transition-all group-hover:p-5 inset-0 h-full w-full'
+          className="object-cover absolute transition-all group-hover:p-5 inset-0 h-full w-full"
         />
+        <div className="absolute inset-0 bg-shade opacity-20 transition-all group-hover:opacity-95 bg-gradient-to-b group-hover:from-accent to-brand"></div>
         <div
-          className='absolute inset-0 bg-shade opacity-20 transition-all group-hover:opacity-95 bg-gradient-to-b group-hover:from-accent to-brand'></div>
-        <div
-          className='transition-all text-center text-white font-heading tracking-wider pb-10 relative px-5 group-hover:pb-40'
+          className="transition-all text-center text-white font-heading tracking-wider pb-10 relative px-5 group-hover:pb-40"
           style={{ textShadow: '0 0 5px black' }}>
-          <p className='uppercase text-sm mb-2'>{meta}</p>
-          <h3 className='text-2xl font-bold'>{title}</h3>
+          <p className="uppercase text-sm mb-2">{meta}</p>
+          <h3 className="text-2xl font-bold">{title}</h3>
         </div>
       </article>
     </a>
--- src/sections/Work.tsx
+++ src/sections/Work.tsx
@@ -8,7 +8,7 @@ export default function () {
         <h2 className="text-white text-4xl lg:text-5xl font-bold tracking-wide">Work</h2>
         <p className="text-content">Check out our latest implemented projects!</p>
       </div>
-      <div className="flex flex-col lg:flex-row gap-10 items-start justify-between mt-10">
+      <div className="flex flex-col md:grid grid-cols-3 gap-10 items-start mt-10">
         <Project title="Web app Buktio" meta="Development" url="https://buktio.com" image="/buktio.png" />
         <Project title="Browser game Skyhavoc" meta="Development" url="https://skyhavoc.com" image="/skyhavoc.png" />
         <Project
@@ -17,6 +17,8 @@ export default function () {
           url="https://apps.apple.com/us/app/babinio/id6443568887"
           image="/babinio.png"
         />
+        <Project title="Web app CoverMy" meta="Development" url="https://app.covermy.com.au" image="/covermy.png" />
+        <Project title="Web app Brokerfy" meta="Development" url="https://brokerfy.ro" image="/brokerfy.png" />
       </div>
     </section>
   );
