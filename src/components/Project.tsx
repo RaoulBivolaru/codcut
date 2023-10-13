34d3f056c2d52c240e287bc3e6fa0e842ddcdd83
Raoul Bivolaru <contact@codcut.com>
2023-10-13 14:21:09 +0300

    fix image

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -21,7 +21,7 @@ const Project: FC<ProjectProps> = props => {
       <article className="w-full h-96 relative flex justify-center items-end rounded-xl overflow-hidden">
         {React.cloneElement(image, { className: 'absolute transition-all inset-0 h-full w-full' })}
         <div className="absolute inset-0 bg-gradient-to-t from-shade/10 to-transparent transition-all group-hover:bg-shade/20"></div>
-        <div className="transition-height text-center text-white h-28 font-heading tracking-wider relative px-5 group-hover:h-full w-full backdrop-blur-sm bg-shade/30 flex flex-col justify-center">
+        <div className="absolute transition-height text-center text-white h-28 font-heading tracking-wider relative px-5 group-hover:h-full w-full backdrop-blur-sm bg-shade/30 flex flex-col justify-center">
           <p className="uppercase text-sm mb-2">{meta}</p>
           <h3 className="text-2xl font-bold">{title}</h3>
         </div>
